09d0f7ef5f83a55eec7d7be604b1e7684a8d52e5
TianMars <137128872+TianMars@users.noreply.github.com>
2023-06-20 12:56:08 +1000

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ moneyPerClickCost: 10
 
     function gainMoney() {
         gameData.money += gameData.moneyPerClick
-        document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Earned"
+        document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Gained"
 }
 
     function buymoneyPerClick() {
