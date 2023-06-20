d567e4426e036b7591bc42cc0462ae54e8d6fcc3
TianMars <137128872+TianMars@users.noreply.github.com>
2023-06-20 13:02:58 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-    var gameData = {
-money: 0,
-moneyPerClick: 1,
-moneyPerClickCost: 10
-
-}
-
-
-    function gainMoney() {
-        gameData.money += gameData.moneyPerClick
-        document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Gained"
-}
-
-    function buymoneyPerClick() {
-  if (gameData.money >= gameData.moneyPerClickCost) {
-        gameData.money -= gameData.moneyPerClickCost
-        gameData.moneyPerClick += 1;
-        gameData.moneyPerClickCost *= 2
-        document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Earned"
-        document.getElementById("perClickUpgrade").innerHTML = "Fundraise (Currently Level " + gameData.moneyPerClick + ") Cost: $" + gameData.moneyPerClickCost 
-      }
-      
-}
-var mainGameLoop = window.setInterval(function() {
-    gainMoney()
-  }, 1000)
-
-  var saveGameLoop = window.setInterval(function() {
-    localStorage.setItem("idleGameSave", JSON.stringify(gameData))
-  }, 15000)
+var gameData = {
+  money: 0,
+  moneyPerClick: 1,
+  moneyPerClickCost: 10
+  
+  }
+  
+  
+      function gainMoney() {
+          gameData.money += gameData.moneyPerClick
+          document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Gained"
+  }
+  
+      function buyMoneyPerClick() {
+    if (gameData.money >= gameData.moneyPerClickCost) {
+          gameData.money -= gameData.moneyPerClickCost
+          gameData.moneyPerClick += 1;
+          gameData.moneyPerClickCost *= 2
+          document.getElementById("moneyGained").innerHTML = "$" + gameData.money + " Gained"
+          document.getElementById("perClickUpgrade").innerHTML = "Fundraise (Currently Level " + gameData.moneyPerClick + ") Cost: $" + gameData.moneyPerClickCost 
+        }
+        
+  }
+  var mainGameLoop = window.setInterval(function() {
+      gainMoney()
+    }, 1000)
+  
+    var saveGameLoop = window.setInterval(function() {
+      localStorage.setItem("idleGameSave", JSON.stringify(gameData))
+    }, 15000)
+  
